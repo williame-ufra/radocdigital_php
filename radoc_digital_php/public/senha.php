5fc3a27b760699d4c6ac70333b563611d1003674
Carleyson <158765168+Carleyson@users.noreply.github.com>
2024-04-10 00:15:01 -0300

    Delete radoc_digital_php directory

--- radoc_digital_php/public/senha.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// temporário: apenas para gerar o hash da senha
-// gerar uma senha com uma função nativa do php chamada password hash
-echo password_hash('senha1', PASSWORD_DEFAULT);
--- radoc_digital_php/scripts/404.php
+++
@@ -1,4 +0,0 @@
-<div class="d-flex mt-5 justify-content-center">
-    <h3>ERRO 404</h3>
-    <p class="text-danger">Página não encontrada</p>
-</div>
